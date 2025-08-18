1f5bb94b46c540d674ded01234aa21f5522268d7
scofield <talesscof@gmail.com>
2025-08-18 20:35:15 +0000

    Cards added

---
+++ my-app/app/components/cardList/CardList.jsx
@@ -0,0 +1,23 @@
+// import Pagination/
+
+import styles from "./cardList.module.css";
+import Image from "next/image";
+import Card from "../card/Card";
+import { Pagination } from "../pagination/Pagination";
+
+const CardList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+      <Pagination/>
+    </div>
+  );
+};
+
+export default CardList;
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./homePage.module.css"
 import Featured from "./components/featured/Feature";
 import CategoryList from "./components/categoryList/CategoryList";
+import CardList from "./components/cardList/CardList";
+import Menu from "./components/Menu/Menu";
 
 export default function Home() {
   return (
     <div>
       <Featured/>
       <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
     </div>
   );
 }
