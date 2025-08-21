b8b0cafe003c8a2ff2b3da23740e795b8b293c08
scofield <talesscof@gmail.com>
2025-08-21 12:35:31 +0000

    Auth

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/footer";
 import { ThemeContextProvider } from "./context/ThemeContext";
-import ThemeProvider from "./providers/ThemeProvider";
+import ThemeProvider from "./src/providers/ThemeProvider";
+import AuthProvider from "./src/providers/AuthProvider";
 
 const inter = Inter({ subset: ["latin"] });
 
@@ -16,17 +17,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="br-pt">
       <body className={inter.className}>
-        <ThemeContextProvider>
-          <ThemeProvider>
-          <div className="container">
-            <div className="wrapper">
-              <Navbar />
-              {children}
-              <Footer />
-            </div>            
-          </div>
-          </ThemeProvider>
-        </ThemeContextProvider>
+        <AuthProvider>
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
+              </div>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
